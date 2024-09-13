2537a52c3a11838aed11785bc2aa99e5d69967f4
Adrien Volery <pd52nbo@eduvaud.ch>
2024-09-13 14:41:36 +0200

    Refactor : si le nombre est plus grand que 50, fin.

--- src/break.js
+++ src/break.js
@@ -1,8 +1,13 @@
 const numbers = [];
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
+
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    if (numbers[i] > 50) {
+        console.log('Plus grand que 50, fin de la boucle. ');
+        break;
+    }
 }
